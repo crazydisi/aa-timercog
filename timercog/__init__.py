90e76a8947367b901bac148404715ab040b2862c
CrazyDisi <crazydisi-github@disi.info>
2025-11-14 17:54:34 +0100

    update version to 0.0.2

--- timercog/__init__.py
+++ timercog/__init__.py
@@ -2,7 +2,7 @@
 Timer Cog - Discord bot extension for aa-structuretimers
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __title__ = "Timer Cog"
 
 default_app_config = "timercog.apps.TimerCogConfig"
